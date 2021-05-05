aa934555752a626e23497e4678f5e1349f7e9a75
benkolilla <66418879+benkolilla@users.noreply.github.com>
2021-05-06 00:03:28 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-//the base of the messages in 3 separate arrays
-    const arr1 = ['one', 'two', 'three'];
-    const arr2 = ['first', 'second, third'];
-    const arr3 = ['1', '2', '3'];
+//container array for messages 
+let message1 = ['You are not weak.','Dont judge yourself by what others did to you.', 'We all make mistakes.', 'What hurts can be healed.', 'The secret of getting ahead is getting satarted.'];
+let message2 = ['You are not your mistake.', 'Dont let your fear own you. Own yourself.', 'Falling down is an accident, staying down is a choice.', 'If you are going through hell kepp going.'];
+let message3 = ['We keep fighting.', 'Care for yourself as much as you care for others.', 'People like us, we are brave.', 'You will never regret being kind.'];
+
+//generate random message  
+const GenerateRandomMessage = (arr1, arr2, arr3)=>{
+    let message = []; 
 
     let randIndex1 = Math.floor(Math.random()*arr1.length);
     let randIndex2 = Math.floor(Math.random()*arr2.length);
@@ -11,11 +15,17 @@
     let randMessage2 = arr2[randIndex2];
     let randMessage3 = arr3[randIndex3];
 
-    let message = [];
-
     message.push(randMessage1);
     message.push(randMessage2);
     message.push(randMessage3);
 
+    message.unshift("'");
+    message.push("'");
 
-console.log(message)
+    return message.join(' ');
+}
+
+//add new message to arrays
+
+console.log(GenerateRandomMessage(message1,message2,message3))
+
